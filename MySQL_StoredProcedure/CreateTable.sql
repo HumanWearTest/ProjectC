f409330ddd10bf33f5d2d45cafdba85dd832ad31
kurihara <kurihara>
2024-12-11 14:44:38 +0900

    make sql create table

--- MySQL_StoredProcedure/CreateTable.sql
+++ MySQL_StoredProcedure/CreateTable.sql
@@ -1,3 +1,5 @@
+
+-- 顧客マスター
 CREATE TABLE CustomerMaster(
     customer_num            CHAR(5)     NOT NULL,
     last_name               VARCHAR(5)  NOT NULL,
@@ -15,5 +17,54 @@ CREATE TABLE CustomerMaster(
     PRIMARY KEY(customer_num)
 );
 
+-- 支店
+CREATE TABLE Branch(
+    branch_num              CHAR(3),
+    branch_name             VARCHAR(10),
+    PRIMARY KEY(branch_num )
+);
+
+-- 大分類
+CREATE TABLE Category(
+    category_num            CHAR(2),
+    category_name           VARCHAR(10),
+    PRIMARY KEY(category_num)
+);
+
+-- 商品マスター
+CREATE TABLE ItemMaster(
+    item_num                CHAR(5)         NOT NULL,
+    category_num            CHAR(2)         NOT NULL,
+    manufacturer            VARCHAR(10)     NOT NULL,
+    product_name            VARCHAR(10)     NOT NULL,
+    unit_price              INT             NOT NULL,
+    stock_quantity          INT             NOT NULL,
+    PRIMARY KEY(item_num),
+    FOREIGN KEY(category_num)
+        REFERENCES Category(category_num)
+);
+
+
+-- 受注データ
+CREATE TABLE OrderData(
+    order_num               INT             NOT NULL,
+    order_date              DATE            NOT NULL,
+    customer_num            CHAR(5)         NOT NULL,
+    item_num                CHAR(5)         NOT NULL,
+    unit_price              INT             NOT NULL,
+    quantity                INT             NOT NULL,
+    branch_num              CHAR(3)         NOT NULL,
+    PRIMARY KEY(order_num),
+    FOREIGN KEY(customer_num)
+        REFERENCES CustomerMaster(customer_num),
+    FOREIGN KEY(item_num)
+        REFERENCES  ItemMaster(item_num),
+    FOREIGN KEY(branch_num)
+        REFERENCES Branch(branch_num)
+);
+
+
+
+-- 列変更例
 ALTER TABLE CustomerMaster
 MODIFY points INT NOT NULL;
